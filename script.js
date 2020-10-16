38af335da7678904642e501949c94e00d4b95830
Your Name <you@example.com>
2020-10-16 12:26:18 +0300

    changes made on all files

---
+++ script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+
+   $('#design').click(function(){
+   $('#design  > p').toggle();
+       $('.colomn').toggle();
+})
+
+  $('#development').click(function(){
+   $('#development').toggle();
+    $('.column').toggle();
+})
+
+$('#productdevelopment').click(function(){
+$('#productdevelopment').toggle();
+$('.column').toggle();
+})
+})
+
+
